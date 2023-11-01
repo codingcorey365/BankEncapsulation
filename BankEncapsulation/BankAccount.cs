c7ed25ef1ba7cefb2841fc86b5c12393aa1f1726
Corey Schroeder <codingcorey365@gmail.com>
2023-11-01 16:13:46 -0500

    final commit to git

--- BankEncapsulation/BankAccount.cs
+++ BankEncapsulation/BankAccount.cs
@@ -9,30 +9,16 @@ namespace BankEncapsulation
     public class BankAccount //public class
     {
         private double balance = 0; //private variable
-
         public void DepositMethod(double depositedAmount) //public method with in BankAccount Class
         {
             balance = depositedAmount += balance;
         }
-
-        public double GetBalance()
+        public double GetBalance()//public method with in BankAccount Class
         {
            return balance;
         }
 
-        public void ExitProgram()
-        {            
-            Console.WriteLine("Do you want to exit the program?");
-            var input = Console.ReadLine();
-            if(input == "y")
-            {
-                Console.WriteLine("exiting");
-            }
-            else 
-            {
-                Console.WriteLine("okay, continue");
-            }
-        }
+        
 
     }
 }
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -14,67 +14,58 @@
         {
 
             BankAccount coreysBankAccount = new BankAccount();
-
-
-
+            
             Console.WriteLine("You are at an ATM, would you like to make a deposit?");
 
             string? answerToDepositQuestion = Console.ReadLine();
-
+            
+            
             if (answerToDepositQuestion == null || answerToDepositQuestion == "")
             {
                 Console.WriteLine("Not a valid response. Next time try to type a proper response. Have a great day!");
-                
+
             }
             else if ((answerToDepositQuestion == "no") || (answerToDepositQuestion == "n"))
             {
-                Console.WriteLine("Okay, maybe next time!");                
+                Console.WriteLine("Okay, maybe next time!");
             }
             else if ((answerToDepositQuestion == "yes") || (answerToDepositQuestion == "y"))
             {
                 Console.WriteLine("Great! Enter in how much you would like to deposit to the account.");
                 //double depositAmount = Convert.ToDouble(Console.ReadLine()); do not need
-            }
-            else
-            {
-                Console.WriteLine("go go gadget");
-            }
 
-            //-------------------------------------------------------------------------------
-
-            if (double.TryParse(Console.ReadLine(), out var depositAmount))
-            {
-                Console.WriteLine("I will now add the deposit amount into your account");
-
-                coreysBankAccount.DepositMethod(depositAmount);
-
-                Console.WriteLine("Would you like to see your new bank balance?");
-                string? answerToGetBalanceQuestion = Console.ReadLine();
-                if (answerToGetBalanceQuestion == null || answerToGetBalanceQuestion == "")
+                if (double.TryParse(Console.ReadLine(), out var depositAmount))
                 {
-                    Console.WriteLine("Not a valid response. Next time try to type a proper response. Have a great day!");
+                    Console.WriteLine("I will now add the deposit amount into your account");
 
+                    coreysBankAccount.DepositMethod(depositAmount);
+
+                    //insert nested if statement
+                    Console.WriteLine("Would you like to see your new bank balance?");
+                    string? answerToGetBalanceQuestion = Console.ReadLine();
+                    if (answerToGetBalanceQuestion == null || answerToGetBalanceQuestion == "")
+                    {
+                        Console.WriteLine("Not a valid response. Next time try to type a proper response. Have a great day!");
+                    }
+                    else if ((answerToGetBalanceQuestion == "yes") || (answerToGetBalanceQuestion == "y"))
+                    {
+                        Console.WriteLine($"Understood! Your new balance after depositing ${depositAmount} is ...{coreysBankAccount.GetBalance()}");
+                                                
+                    }
+                    else if ((answerToDepositQuestion == "no") || (answerToDepositQuestion == "n"))
+                    {
+                        Console.WriteLine("Okay, maybe next time!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid response. Next time try to type a proper response. Have a great day!");
+                    }
                 }
-                else if ((answerToGetBalanceQuestion == "yes") || (answerToGetBalanceQuestion == "y"))
-                     {
-                         Console.WriteLine($"Understood! Your new balance after depositing ${depositAmount} is ...");
-                         //enter method
-                         Console.WriteLine($"${coreysBankAccount.GetBalance()}");
-                     }
-                else if ((answerToDepositQuestion == "no") || (answerToDepositQuestion == "n"))
-                     {
-                         Console.WriteLine("Okay, maybe next time!");
-                     }
             }
             else
             {
                 Console.WriteLine("Not a valid response. Next time try to type a proper response. Have a great day!");
-
             }
-
-            
-
-
         }
     }
 }
